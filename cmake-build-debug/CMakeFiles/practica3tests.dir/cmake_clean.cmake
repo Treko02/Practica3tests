887823fad36f997bc77b87a4b27ed9bf8be12335
Treko02 <diego.tsolana@hotmail.es>
2022-03-23 10:34:31 +0100

    gg

---
+++ cmake-build-debug/CMakeFiles/practica3tests.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/practica3tests.dir/main.cpp.obj"
+  "libpractica3tests.dll.a"
+  "practica3tests.exe"
+  "practica3tests.exe.manifest"
+  "practica3tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/practica3tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
